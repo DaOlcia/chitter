7b5b2a64da9ac5ccbbb0184c34f7349c3d889d28
KEVN <troostkevin8@gmail.com>
2023-03-22 13:11:05 +0100

    wachtwoord encrypten test 1

--- registratie.php
+++ registratie.php
@@ -1,12 +1,14 @@
 <?php
     require_once "conn.php";
 
-    $username = $_POST["username"];
-    $password = $_POST["PASSWORD"];
+    strip_tags($_POST["username"];)
+    Strip_tags($_POST["password"];)
 
     $insert_user = $conn->prepare("INSERT INTO Account(username, password) VALUES(:username, :password)");
     $insert_user->bindParam(":username", $username);
-    $insert_user->bindParam(":password", $password);
-    $insert_user->execute();
+    $insert_user->bindParam(":password", $hashed_password);
 
+    $password_difficulty = ['difficulty' => 11];
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT, $password_difficulty);
+    $stmt->execute(header("location: registreren_succes.html"));
     ?>
