960d3363adf8302bac6c6ee44d1b150f22f33d05
KEVN <troostkevin8@gmail.com>
2023-03-24 15:36:39 +0100

    .

--- index.php
+++ index.php
@@ -1,62 +1,57 @@
+<?php require_once('show_all_tweets.php') ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Chirpify</title>
-    <link rel="stylesheet" href="Main.css">
+  <meta charset="UTF-8">
+  <title>Chirpify</title>
+  <link rel="stylesheet" href="Main.css">
 </head>
+
 <body>
-<section class="homepage">
-  <div class="sidebar">
-    <img class="logo" src="img/chitter.png" width="48px" height="48px" alt="chitterlogo">
-    <ul>
-      <li><a href="index.php">Home</a></li>
-      <li><a href="index.php">Profiel</a></li>
-      <li><a class="btn" href="">Tweet</a></li>
-    </ul>
-    <div class="spacer"></div>
-    <div class="account">
-      <img src="img/chitter.png" width="38px" height="38px" alt="chitterlogo">
-      <div class="user-details">
-        <p>John doe</p>
-        <p>@johndoe</p>
-      </div>
-      <div class="menuicon">
-        <span></span>
-        <span></span>
-        <span></span>
+  <section class="homepage">
+    <div class="sidebar">
+      <img class="logo" src="img/chitter.png" width="48px" height="48px" alt="chitterlogo">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="index.php">Profiel</a></li>
+      </ul>
+
+
+      <div class="spacer"></div>
+
+      <div class="account">
+        <img src="img/chitter.png" width="38px" height="38px" alt="chitterlogo">
+        <div class="user-details">
+          <p>John doe</p>
+          <p>@johndoe</p>
+        </div>
+        <div class="menuicon">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="feed">
-    <div class="write-a-post">
-    <img src="img/chitter.png" width="38px" height="38px" alt="chitterlogo">
-      <div class="tweet-input" data-placeholder="Typ iets..." contenteditable></div>
+
+    <div class="feed">
+      <div class="write-a-post">
+        <img src="img/chitter.png" width="38px" height="38px" alt="chitterlogo">
+        <div class="tweet-input" data-placeholder="Typ iets..." contenteditable></div>
+      </div>
+      <div class="post"></div>
     </div>
-    <div class="tweets">
-    
-    
-    <?php 
-require_once "conn.php";
 
-// Bereid je voor om alle tweets te pakken .
-$get_all_tweets = $conn->prepare("SELECT * from tweets");
 
-//Voer het uit
-$get_all_tweets->execute();
-$tweets = $get_all_tweets->fetchAll();
 
-//Pak alle tweets en laat ze een voor een zien.
-foreach ($tweets as $tweet) {
-    echo "<div class='post'>".  $tweet["Content"] ." </div> ";
-}
-?>
-  </div>
-  <div class="featured">
-  <a class="login" href="">Login</a>
-  <a class="registratie" href="registratie_formulier.php">Registreren</a>
 
-  </div>
-</section>
+      <div class="featured">
+        <a class="login" href="">Login</a>
+        <a class="registratie" href="registratie_formulier.php">Registreren</a>
+      </div>
+  </section>
 </body>
+
 </html>
--- registratie.php
+++ registratie.php
@@ -16,13 +16,33 @@
     $insert_user->bindParam(":email", $email);
     $insert_user->bindParam(":gebruikersnaam", $gebruikersnaam);
     $insert_user->bindParam(":wachtwoord", $hashed_wachtwoord);
-
-    $password_difficulty = ['difficulty' => 11];
-    $hashed_wachtwoord = password_hash($wachtwoord, PASSWORD_BCRYPT, $password_difficulty);
+  
+    $sql = "SELECT COUNT(*) AANTAL FROM account WHERE email = :un";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(["un" => $_POST['email']]);
+    $aantal = $stmt->fetchColumn();
+    //-----------------------------------------------------
     
-
-    $_SESSION["gebruikersnaam"] = $gebruikersnaam;
-    $insert_user->execute(header("location: logged_in_user.php"));
-
+    // begin van if stmt
+    if ($aantal == 1) {
+        // $_SESSION['err']; // = "<h3 style='color:#900'>Deze gebruikersnaam is al aanwezig in de database. Probeer opnieuw ///De h1 tag doet niks</h3>";
+        header("location: retry_register.php");
+    } else {
+        // $_SESSION['err']; //  = "<h3>REGISTRATIE VOLTOOID</h3>";
+        // dit is om de ingetypte data in de database te zetten
+        $insert_user = $conn->prepare("INSERT INTO users(voornaam,achternaam,email,gebruikersnaam, wachtwoord) VALUES( :voornaam, :achternaam, :email, :gebruikersnaam, :wachtwoord)");
+        $insert_user->bindParam(":voornaam", $voornaam);
+        $insert_user->bindParam(":achternaam", $achternaam);
+        $insert_user->bindParam(":email", $email);
+        $insert_user->bindParam(":gebruikersnaam", $gebruikersnaam);
+        $insert_user->bindParam(":wachtwoord", $hashed_wachtwoord);
+        //-----------------------------------------------------
     
+        //voor betere incryptie// // cost 12 = default incryptie // 
+        $password_difficulty = ['difficulty' => 11];
+        $hashed_wachtwoord = password_hash($wachtwoord, PASSWORD_BCRYPT, $password_difficulty);
+        $_SESSION["gebruikersnaam"] = $gebruikersnaam;
+        // strip_tags($_POST['username']);
+        $stmt->execute(header("location: logged_in_user.php")); 
+    };
 ?>
