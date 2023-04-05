3ea855fccdfadce1c3b37f0a36546d30d79868bd
KEVN <troostkevin8@gmail.com>
2023-04-05 13:51:06 +0200

    .

--- profiel_bewerken.php
+++ profiel_bewerken.php
@@ -2,64 +2,55 @@
 
 session_start(); 
 require_once "conn.php";
-?> 
+?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <title>Chirpify</title>
-    <link rel="stylesheet" href="main.css">
+  <title>Chirpify</title>
+  <link rel="stylesheet" href="main.css">
 </head>
+
 <body>
-<section class="homepage homeregistratie">
-  <div class="sidebar">
-    <img class="logo" src="img/chitter.png" width="48px" height="48px" alt="chitterlogo">
-    <ul>
-      <li><a href="index.php">Home</a></li>
-      <li><a href="index.php">Profiel</a></li>
-    </ul>
-    <div class="spacer"></div>
+  <section class="homepage homeregistratie">
+    <div class="sidebar">
+      <img class="logo" src="img/chitter.png" width="48px" height="48px" alt="chitterlogo">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="index.php">Profiel</a></li>
+      </ul>
+      <div class="spacer"></div>
     </div>
-  </div>
+    </div>
 
     <div class="feed">
-    <h1>Profiel bijwerken</h1>
-    <form action="" method="post">
-    
-    <?php 
+      <h1>Profiel bijwerken</h1>
+      
+
+        <?php 
        $gebruiker = $_SESSION['gebruikersnaam']; 
-      $sql= "SELECT * FROM users WHERE gebruikersnaam ='$gebruiker'"; 
+       $query= $conn->prepare("SELECT * FROM users WHERE Gebruikersnaam = :gebruikersnaam" ); 
+       $query->bindParam(':gebruikersnaam', $gebruiker);
+        $query->execute();
 
-      $resultaat = mysql_query($connection,$sql)
-       
+        $resultaat = $query->fetchAll();
+        foreach ($resultaat as $gebruiker) {
+        echo '<div class="post"><form action="profiel_bewerken_vervolg.php" method="post">'.  $gebruiker["Gebruikersnaam"] .'                      
+        <label for="gebruikersnaam">Gebruikersnaam</label>
+        <input type="text" id="gebruikersnaam" name="gebruikersnaam">
+        <label for="nieuwe_gebruikersnaam">Nieuwe gebruikersnaam</label>
+        <input type="text" id="nieuwe_gebruikersnaam" name="nieuwe_gebruikersnaam">
+        <label for="wachtwoord" name="wachtwoord">Wachtwoord</label>
+        <input type="password" id="wachtwoord" name="wachtwoord">
+        <input type="submit" value="Profiel bewerken" name="profielbewerken1">
+        </form></div> ';}
 
-      if ( $resultaat){
-            if (mysql_num_rows( $resultaat)>0) {
-                 while($row = mysql_fetch_array($resultaat)) {
-                     // print_r($row);
-                     ?>
-                     <label for="voornaam">Voornaam</label>
-                          <input type="text" id="voornaam" name="voornaam" value="<?php echo"$_SESSION[gebruikersnaam]"; ?>">
-                     <label for="achternaam">Achternaam</label>
-                         <input type="text" id="achternaam" name="achternaam">
-                     <label for="email">Email</label>
-                         <input type="email" id="email" name="email" >
-                     <label for="gebruikersnaam">Gebruikersnaam</label>
-                         <input type="text" id="gebruikersnaam" name="gebruikersnaam" >
-                     <label for="wachtwoord">Wachtwoord</label>
-                         <input type="password" id="wachtwoord" name="wachtwoord" >
-                    <input type="submit" value="Profiel bewerken">
-  </div>
-</section>
-</body>
-</html>
-                     <?php 
-                 }
-            }
-      }
 
+    
 ?>
-        
 
+</body>
 
+</html>
