13e6d86e59ddf12722fe77d4e440726ee3dd15ec
DaGabyy <20060929011d@gmail.com>
2023-04-05 14:56:12 +0200

    Update tweet_posten.php

--- tweet_posten.php
+++ tweet_posten.php
@@ -8,12 +8,10 @@
     // Of gelijk session [id]
 
         
-        $insert_tweets = $conn->prepare("INSERT INTO tweets(Content) VALUES( :content)");
+        $insert_tweets = $conn->prepare("INSERT INTO tweets(Content,account_id) VALUES( :content,:account_id )");
         $insert_tweets->bindParam(":content", $content);
-    
-         
-        $insert_tweets = $conn->prepare ("INSERT INTO tweets(account_id) VALUES (:account-id)");
         $insert_tweets->bindParam(":account_id", $account_id);
+
 
         $insert_tweets->execute(header("location: logged_in_user.php"));
     
