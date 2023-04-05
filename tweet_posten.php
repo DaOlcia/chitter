a0bc9574c8c5ca2b665ee86f8603dd6ff1cae8bf
DaGabyy <20060929011d@gmail.com>
2023-04-05 14:39:25 +0200

    koppelen

--- tweet_posten.php
+++ tweet_posten.php
@@ -3,11 +3,18 @@
     require_once "conn.php";
     
     $content = strip_tags($_POST["content"]);
+    $account_id = "$SESSION[id]"; 
+    // Ik weet niet zeker of je user(tabelnaam)en dan id moet schrijven 
+    // Of gelijk session [id]
 
         
         $insert_tweets = $conn->prepare("INSERT INTO tweets(Content) VALUES( :content)");
         $insert_tweets->bindParam(":content", $content);
     
+         
+        $insert_tweets = $conn->prepare ("INSERT INTO tweets(account_id) VALUES (:account-id)");
+        $insert_tweets->bindParam(":account_id", $account_id);
+
         $insert_tweets->execute(header("location: logged_in_user.php"));
     
     ?>
